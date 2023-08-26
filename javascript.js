045d86c9a496f5b5c82ecf26c86bb6808facd8d9
Hariprasathr003 <143156763+Hariprasathr003@users.noreply.github.com>
2023-08-26 11:04:15 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", hari() {
+    const ctaButton = document.getElementById("ctaButton");
+
+    ctaButton.addEventListener("click", hari() {
+        alert("Thank you for your interest!");
+    });
+});
